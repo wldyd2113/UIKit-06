dcba2fdaee08baf19221c55e4a0fb9dc98337c09
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-07 16:25:44 +0900

    Add MyPlayground

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let theAnswerToTheUltimateQuestion = 42
+
+//var greeting = "Hello, playground"
+
+var restaurantRating: Double = 3
+//restaurantRating = "Good"
+
+let a = 12
+let b = 12.0
+let c = Double(a) + b
+
+var d = 1
+d += 2 // d = d + 2
+d -= 1 // d = d - 1
+
+let greeting = "Good" + " Morning"
+
+let rating = 3.5
+var ratingResult = "The restaurant rating is " + String(rating)
+ratingResult = "The restaurant rating is \(rating)"
+print(ratingResult)
