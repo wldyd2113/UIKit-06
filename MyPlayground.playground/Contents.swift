1cae6a67e23d98b17caa8a10a19afeac92d5f136
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-07 17:15:24 +0900

    Range and loop:

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -68,3 +68,30 @@ if let spouseTempVar = spouseName {
 } else {
     print("No one")
 }
+
+let myRange = 10...20
+let myRange2 = 10..<20
+
+for number in myRange {
+    print(number)
+}
+
+for number in myRange2 {
+    print(number)
+}
+
+for number in (0...5).reversed() {
+    print(number)
+}
+
+var y = 0
+while y<50 {
+    y += 5
+    print("y is \(y)")
+}
+
+var x = 0
+repeat {
+    x += 5
+    print("x is \(x)")
+} while x < 50
