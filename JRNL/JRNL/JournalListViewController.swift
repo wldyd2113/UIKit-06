5685177828d8eec4e92dcd43ae8c6e2f33511af6
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-09 15:35:55 +0900

    TableviewDataSource,Delegate, tableCell

---
+++ JRNL/JRNL/JournalListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  JRNL
+//
+//  Created by 차지용 on 5/7/24.
+//
+
+import UIKit
+
+class JournalListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath)
+    }
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
