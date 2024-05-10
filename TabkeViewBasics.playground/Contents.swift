96fa5c75ac17b06e227e2402a1a14312dbf547d9
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-10 10:07:07 +0900

    TableViewBasics

--- TabkeViewBasics.playground/Contents.swift
+++ TabkeViewBasics.playground/Contents.swift
@@ -1,4 +1,10 @@
 import UIKit
 import PlaygroundSupport
 
-var greeting = "Hello, playground"
+class TabkeViewExampleController: UIViewController {
+    var tableView: UITableView?
+    var journalEntries: [[String?]] = [
+        ["sun.max", "9 Apr 2023", "Nice weather today"],
+        ["cloud.rain", "10 Apr 2023", "Heavy rain today"],
+        ["cloud.sun", "11 Apr 2023", "It's cloudy out"]]
+}
