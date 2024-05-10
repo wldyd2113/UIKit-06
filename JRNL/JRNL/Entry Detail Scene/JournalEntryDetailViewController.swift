966299c5eb1682f4c5b642aab2af0a3766992ffe
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-10 17:43:59 +0900

    Data Binding

--- JRNL/JRNL/Entry Detail Scene/JournalEntryDetailViewController.swift
+++ JRNL/JRNL/Entry Detail Scene/JournalEntryDetailViewController.swift
@@ -17,21 +17,26 @@ class JournalEntryDetailViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        dateLabel.text = selectedJournalEntry?.date.formatted(
+            .dateTime.year().month(.wide).day()
+        )
+        titleLabel.text = selectedJournalEntry?.entryTitle
+        bodyTextView.text = selectedJournalEntry?.entryBody
+        photoImageView.image = selectedJournalEntry?.photo
         
     }
-
+/*
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 7
     }
-
+*/
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
@@ -41,6 +46,7 @@ class JournalEntryDetailViewController: UITableViewController {
         return cell
     }
     */
+
 
     /*
     // Override to support conditional editing of the table view.
