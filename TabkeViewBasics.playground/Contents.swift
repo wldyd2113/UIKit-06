47eca500e7f1d2457c2014c9d548fd82e606519e
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-10 10:22:55 +0900

    TableViewDataSource

--- TabkeViewBasics.playground/Contents.swift
+++ TabkeViewBasics.playground/Contents.swift
@@ -1,10 +1,27 @@
 import UIKit
 import PlaygroundSupport
 
-class TabkeViewExampleController: UIViewController {
+class TableViewExampleController: UIViewController, UITableViewDataSource {
     var tableView: UITableView?
-    var journalEntries: [[String?]] = [
+    var journalEntries: [[String]] = [
         ["sun.max", "9 Apr 2023", "Nice weather today"],
         ["cloud.rain", "10 Apr 2023", "Heavy rain today"],
-        ["cloud.sun", "11 Apr 2023", "It's cloudy out"]]
+        ["cloud.sun", "11 Apr 2023", "It's cloudy out"]
+    ]
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        journalEntries.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let journalEntry = journalEntries[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.image = UIImage(systemName: journalEntry[0])
+        content.text = journalEntry[1]
+        content.secondaryText = journalEntry[2]
+        cell.contentConfiguration = content
+        return cell
+    }
+
 }
