8989cd8bf541e805b34735300e4cef3915c71e2e
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-14 16:37:53 +0900

    지도 스냅샷 추가

--- JRNL/JRNL/Entry Detail Scene/JournalEntryDetailViewController.swift
+++ JRNL/JRNL/Entry Detail Scene/JournalEntryDetailViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MapKit
 
 class JournalEntryDetailViewController: UITableViewController {
     
@@ -13,6 +14,8 @@ class JournalEntryDetailViewController: UITableViewController {
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var bodyTextView: UITextView!
     @IBOutlet var photoImageView: UIImageView!
+    @IBOutlet var mapImageView: UIImageView!
+    
     var selectedJournalEntry: JournalEntry?
 
     override func viewDidLoad() {
@@ -23,6 +26,7 @@ class JournalEntryDetailViewController: UITableViewController {
         titleLabel.text = selectedJournalEntry?.entryTitle
         bodyTextView.text = selectedJournalEntry?.entryBody
         photoImageView.image = selectedJournalEntry?.photo
+        getMapSnapshot()
         
     }
 /*
@@ -31,7 +35,28 @@ class JournalEntryDetailViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+ */
+    // MARK: - Private Methods
+    private func getMapSnapshot() {
+        if let lat = selectedJournalEntry?.latitude,
+           let long = selectedJournalEntry?.longitude {
+            let options = MKMapSnapshotter.Options()
+            options.region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lat, longitude: long), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            options.size = CGSize(width: 300, height: 300)
+            options.preferredConfiguration = MKStandardMapConfiguration()
+            let snapShotter = MKMapSnapshotter(options: options)
+            snapShotter.start { snapShot, error in
+                if let snapshot = snapShot {
+                    self.mapImageView.image = snapshot.image
+                } else if let error = error {
+                    print("snapShot error: \(error.localizedDescription)")
+                }
+            }
+        } else {
+            self.mapImageView.image = UIImage(systemName: "map")
+        }
+    }
+/*
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return 7
