76cc28add9ad30dab0c4a008c839ecd20c94f490
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-14 14:14:44 +0900

    위치 정보 업데이트 코드 추가가 아니환경변수 설정 (Info.plist)

---
+++ JRNL/JRNL/Map Screen/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  JRNL
+//
+//  Created by 차지용 on 5/14/24.
+//
+
+//import UIKit
+//import CoreLocation
+//import MapKit
+//class MapViewController: UIViewController, CLLocationManagerDelegate {
+//    
+//    @IBOutlet var mapView: MKMapView!
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//    }
+//    
+//    // MARK: - CLLocationManagerDelegate
+//
+//}
