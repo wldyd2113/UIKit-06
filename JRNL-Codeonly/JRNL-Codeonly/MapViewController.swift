8349580e0caeff20e4d7a4920929550c8532151d
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-14 12:45:07 +0900

    MapView 추가

--- JRNL-Codeonly/JRNL-Codeonly/MapViewController.swift
+++ JRNL-Codeonly/JRNL-Codeonly/MapViewController.swift
@@ -6,13 +6,30 @@
 //
 
 import UIKit
-
+import MapKit
 class MapViewController: UIViewController {
+
+    private lazy var mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        navigationItem.title = "Map"
+        view.addSubview(mapView)
+        
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            mapView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
+    }
     }
     
 
