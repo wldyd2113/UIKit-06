26060e459ff29bb561e3699f364cd240d03b6c4d
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-14 13:38:33 +0900

    위치 정보 업데이트 코드 추가

--- JRNL/JRNL/Add New Entry Screen/AddJournalEntryViewController.swift
+++ JRNL/JRNL/Add New Entry Screen/AddJournalEntryViewController.swift
@@ -6,11 +6,11 @@
 //
 
 import UIKit
-import CoreLocation //위치정보에 관련된것을 가져와서 사용할 수 있음
+import CoreLocation
 
 class AddJournalEntryViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate, CLLocationManagerDelegate {
-    
-    @IBOutlet var titleTextField: UITextField! //@IBOutlet은 옵셔널 Value
+
+    @IBOutlet var titleTextField: UITextField!
     @IBOutlet var bodyTextView: UITextView!
     @IBOutlet var photoImageView: UIImageView!
     @IBOutlet var saveButton: UIBarButtonItem!
@@ -18,48 +18,57 @@ class AddJournalEntryViewController: UIViewController, UITextFieldDelegate, UITe
     @IBOutlet var getLocationSwitch: UISwitch!
     @IBOutlet var getLocationSwitchLabel: UILabel!
     
-    var newJournalEntry: JournalEntry? // 처음부터 없는 값을 만들어서 옵셔널 선언
-    let locationManger = CLLocationManager()
+    
+    var newJournalEntry: JournalEntry?
+    let locationManager = CLLocationManager()
     var currentLocation: CLLocation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         titleTextField.delegate = self
         bodyTextView.delegate = self
-        // Do any additional setup after loading the view.
         updateSaveButtonState()
         
-        locationManger.delegate = self
-        locationManger.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
     }
     
-
+    
     
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) { //
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
         let title = titleTextField.text ?? ""
         let body = bodyTextView.text ?? ""
         let photo = photoImageView.image
-        let rateing = 3
-        newJournalEntry = JournalEntry(rateing: rateing, title: title, body: body, photo: photo)
+        let rating = 3
+        let lat = currentLocation?.coordinate.latitude
+        let long = currentLocation?.coordinate.longitude
+        newJournalEntry = JournalEntry(rateing: rating, title: title,
+                                       body: body, photo: photo,
+                                       latitude: lat, longitude: long)
     }
-    
-    // MARK: -UITextFieldDelegate
+
+    // MARK: - UITextFieldDelegate
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
+    
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         updateSaveButtonState()
         return true
     }
+        
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        updateSaveButtonState()
+    }
     
     // MARK: - UITextViewDelegate
-    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange,
+                  replacementText text: String) -> Bool {
         print("text: \(range.description) \(text)")
         if(text == "\n") {
             textView.resignFirstResponder()
@@ -71,10 +80,38 @@ class AddJournalEntryViewController: UIViewController, UITextFieldDelegate, UITe
     func textViewDidEndEditing(_ textView: UITextView) {
         updateSaveButtonState()
     }
-    //MARK - Methods
+    
+    // MARK: - CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let myCurrentLocation = locations.first {
+            currentLocation = myCurrentLocation
+            getLocationSwitchLabel.text = "Done"
+            updateSaveButtonState()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print("Failed to find user's location: \(error.localizedDescription)")
+    }
+    
+    // MARK: - Methods
     private func updateSaveButtonState() {
         let textFieldText = titleTextField.text ?? ""
         let textViewText = bodyTextView.text ?? ""
-        saveButton.isEnabled = !textFieldText.isEmpty && !textViewText.isEmpty
+        if getLocationSwitch.isOn {
+            saveButton.isEnabled = !textFieldText.isEmpty && !textViewText.isEmpty && currentLocation != nil
+        } else {
+            saveButton.isEnabled = !textFieldText.isEmpty && !textViewText.isEmpty
+        }
+    }
+    
+    @IBAction func getLocationSwitchValueChanged(_ sender: UISwitch) {
+        if getLocationSwitch.isOn {
+            getLocationSwitchLabel.text = "Getting location..."
+            locationManager.requestLocation()
+        } else {
+            currentLocation = nil
+            getLocationSwitchLabel.text = "Get location"
+        }
     }
 }
