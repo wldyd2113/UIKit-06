54751490a03dae8250d2ec060110281f97920236
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-14 14:20:12 +0900

    MapViewController 추가

--- JRNL/JRNL/Map Screen/MapViewController.swift
+++ JRNL/JRNL/Map Screen/MapViewController.swift
@@ -5,18 +5,37 @@
 //  Created by 차지용 on 5/14/24.
 //
 
-//import UIKit
-//import CoreLocation
-//import MapKit
-//class MapViewController: UIViewController, CLLocationManagerDelegate {
-//    
-//    @IBOutlet var mapView: MKMapView!
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        
-//    }
-//    
-//    // MARK: - CLLocationManagerDelegate
-//
-//}
+import UIKit
+import CoreLocation
+import MapKit
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet var mapView: MKMapView!
+    let locationManger = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManger.delegate = self
+        locationManger.requestWhenInUseAuthorization()
+        self.navigationItem.title = "Loading..."
+        locationManger.requestLocation()
+        
+    }
+    
+    // MARK: - CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let myLocation = locations.first {
+            let lat = myLocation.coordinate.latitude
+            let long = myLocation.coordinate.longitude
+            self.navigationItem.title = "Map"
+            mapView.region = setInitialRegion(lat: lat, long: long)
+        }
+    }
+    
+    // MARK: - Methods
+    func setInitialRegion(lat: CLLocationDegrees, long: CLLocationDegrees) -> MKCoordinateRegion {
+        MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lat, longitude: long), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+    }
+
+}
