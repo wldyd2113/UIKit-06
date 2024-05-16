2c0627192f74649f980934e49c1c8b5c2d33ecb3
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-16 14:11:27 +0900

    AddNewEntry 에 RatingView 적용, [fix] RatingView Setup 누락

--- JRNL/JRNL/Add New Entry Screen/AddJournalEntryViewController.swift
+++ JRNL/JRNL/Add New Entry Screen/AddJournalEntryViewController.swift
@@ -17,6 +17,7 @@ class AddJournalEntryViewController: UIViewController, UITextFieldDelegate, UITe
     
     @IBOutlet var getLocationSwitch: UISwitch!
     @IBOutlet var getLocationSwitchLabel: UILabel!
+    @IBOutlet var ratingView: RatingView!
     
     
     var newJournalEntry: JournalEntry?
@@ -43,7 +44,7 @@ class AddJournalEntryViewController: UIViewController, UITextFieldDelegate, UITe
         let title = titleTextField.text ?? ""
         let body = bodyTextView.text ?? ""
         let photo = photoImageView.image
-        let rating = 3
+        let rating = ratingView.rating
         let lat = currentLocation?.coordinate.latitude
         let long = currentLocation?.coordinate.longitude
         newJournalEntry = JournalEntry(rateing: rating, title: title,
