80ebde31e4076696bc53c120dc7d7f6bc024a372
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-16 13:06:49 +0900

    RatingView추가/View 파일이동

---
+++ JRNL/JRNL/View/RatingView.swift
@@ -0,0 +1,20 @@
+//
+//  RatingView.swift
+//  JRNL
+//
+//  Created by 차지용 on 5/16/24.
+//
+
+import UIKit
+
+class RatingView: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
