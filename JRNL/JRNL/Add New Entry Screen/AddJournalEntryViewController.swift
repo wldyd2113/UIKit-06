d83058a9fe0b3f6f43fc37cb7c5d225b31914e8c
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-16 15:27:04 +0900

    AddView 에 이미지피커 추가

--- JRNL/JRNL/Add New Entry Screen/AddJournalEntryViewController.swift
+++ JRNL/JRNL/Add New Entry Screen/AddJournalEntryViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 import CoreLocation
 
-class AddJournalEntryViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate, CLLocationManagerDelegate {
+class AddJournalEntryViewController: UIViewController, UITextFieldDelegate, 
+                                        UITextViewDelegate, CLLocationManagerDelegate, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     @IBOutlet var titleTextField: UITextField!
     @IBOutlet var bodyTextView: UITextView!
@@ -115,5 +116,12 @@ class AddJournalEntryViewController: UIViewController, UITextFieldDelegate, UITe
             currentLocation = nil
             getLocationSwitchLabel.text = "Get location"
         }
+    }
+    
+    @IBAction func getPhoto(_ sender: UITapGestureRecognizer) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate.self
+        imagePickerController.sourceType = .photoLibrary //어디서 가져올지 결정
+        present(imagePickerController, animated: true)
     }
 }
