a081563825cd46adf690d271820db79a25c23796
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-17 16:45:41 +0900

    RatingView 생성

---
+++ JRNL-Codeonly/JRNL-Codeonly/Views/RatingView.swift
@@ -0,0 +1,63 @@
+//
+//  RatingView.swift
+//  JRNL-Codeonly
+//
+//  Created by 차지용 on 5/17/24.
+//
+
+import UIKit
+
+class RatingView: UIStackView {
+    private var ratingButtons: [UIButton] = []
+    var rating = 0
+    private let buttonSize = CGSize(width: 44, height: 44)
+    private let buttonCount = 5
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButton()
+    }
+    
+    private func setupButton() {
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview() //버튼을 지우는 작업
+        }
+        ratingButtons.removeAll() //배열에서 버튼 다 지움
+        let filledStar = UIImage(systemName: "star.fill")
+        let emptyStar = UIImage(systemName: "star")
+        let highlightedStar = UIImage(systemName: "star.fill")?.withTintColor(.red, renderingMode: .alwaysOriginal)
+        
+        for _ in 0..<buttonCount {
+            let button = UIButton()
+            button.setImage(emptyStar, for: .normal)
+            button.setImage(filledStar, for: .selected)
+            button.setImage(highlightedStar, for: .highlighted)
+            button.setImage(highlightedStar, for: [.highlighted, .selected])
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.addTarget(self, action: #selector(ratingButtonTapped(button:)), for: .touchDown)
+            addArrangedSubview(button)
+            ratingButtons.append(button)
+        }
+    }
+    
+    private func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerated() {
+            button.isSelected = index < rating
+        }
+    }
+    
+    @objc func ratingButtonTapped(button: UIButton) {
+        guard let index = ratingButtons.firstIndex(of: button) else {
+            fatalError("The button \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        let selectedRating = index + 1
+        if selectedRating == rating {
+            rating = 0
+        }
+        else {
+            rating = selectedRating
+        }
+    }
+
+}
