80d5351dbf7380b0e0b5c458a23578e6e2a5573f
jiyong <wldyd2113@gmail.com>
2024-07-25 09:41:38 +0900

    Create Socially

---
+++ Socially/Socially/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Socially
+//
+//  Created by 차지용 on 7/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
